c12a15f55b5b1534ec3bdd1c2186d7ac17034f22
thejae-u <31125543+thejae-u@users.noreply.github.com>
2026-01-25 18:27:18 +0900

    update: 초기 세팅

---
+++ _pages/categories.md
@@ -0,0 +1,7 @@
+---
+title: "Posts by Category"
+layout: categories
+permalink: /categories/
+---
+
+Posts sorted by category.
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,13 @@
 var store = [{
+        "title": "C++ random_device",
+        "excerpt":"오류 사항은 contact@thejaeu.com 으로 보내주시기 바랍니다 기본 사용 #include &lt;random&gt; // ... std::random_device seed; std::mt19937 generator(seed()); auto rNumber = generator(); // ... &lt;random&gt; 헤더를 통해 random_device를 초기화 하고 mt19937(유사 난수 생성기)를 해당 random_device로 초기화 함 mt19937에는 operator()가 정의 되어있으며 호출만 하면 난수가 생성이 됨 random number distribution 정수 균등 분포...","categories": ["C++"],
+        "tags": ["c++","cpp","random_device","mt19937"],
+        "url": "/c++/random-device/",
+        "teaser": null
+      },{
         "title": "Sample",
         "excerpt":"   Sample Post   #include &lt;iostream&gt;  int main() {     std::cout &lt;&lt; \"hello world!\\n\"; }  ","categories": ["Test"],
-        "tags": [],
+        "tags": ["Test","First"],
         "url": "/test/sample/",
         "teaser": null
       }]
